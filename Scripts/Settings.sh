1259c2cc7d18bc5d7fc2bd3d749ba7040ea7afe9
zsj9418zsj <140706155+zsj9418zsj@users.noreply.github.com>
2024-11-25 12:10:48 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+# 修改默认IP & 固件名称
+sed -i 's/192.168.1.1/192.168.3.18/g' package/base-files/files/bin/config_generate
+sed -i "s/hostname='.*'/hostname='JDCAX1800'/g" package/base-files/files/bin/config_generate
 #修改默认主题
 sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 #修改immortalwrt.lan关联IP
